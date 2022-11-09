ae809a67b30ac28be84afbbc6539ff1567573259
beni safangat <benisafangat@gmail.com>
2022-11-09 13:26:51 +0700

    learn read images and videos

---
+++ read.py
@@ -0,0 +1,22 @@
+import cv2 as cv
+
+# Reading Image
+
+img = cv.imread('photos/cat_large2.jpg')
+cv.imshow('Cat', img) #menampilkan gambar sebagai jendela baru
+cv.waitKey(0)
+
+
+# Reading Video
+"""
+capture = cv.VideoCapture('videos/dog.mp4')
+while True:
+    isTrue, frame = capture.read()
+    cv.imshow('video', frame)
+
+    if cv.waitKey(20) & 0xFF == ord('d'):
+        break
+
+capture.release()
+cv.destroyAllWindows()
+"""
